ef610792473ee1aef904569f7a1e00b711aab119
Vlad Borovtsov <borovcov.vlad@gmail.com>
2017-02-23 19:45:23 +0700

    paint view initial import

---
+++ OSX_ObjectiveC/SimplePainting/SimplePainting/PaintView.h
@@ -0,0 +1,13 @@
+//
+//  PaintView.h
+//  SimplePainting
+//
+//  Created by Vlad Borovtsov on 23.02.17.
+//  Copyright © 2017 Vlad Borovtsov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PaintView : NSView
+
+@end
---
+++ OSX_ObjectiveC/SimplePainting/SimplePainting/PaintView.m
@@ -0,0 +1,19 @@
+//
+//  PaintView.m
+//  SimplePainting
+//
+//  Created by Vlad Borovtsov on 23.02.17.
+//  Copyright © 2017 Vlad Borovtsov. All rights reserved.
+//
+
+#import "PaintView.h"
+
+@implementation PaintView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
